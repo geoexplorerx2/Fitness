0888f3855edcc54e828924269a49bb4b34cfb01b
farshad nabizadeh <farshadnabizadeh1993@gmail.com>
2026-07-22 12:51:51 +0330

    Signin Created

--- app/[locale]/accout/signup/page.tsx
+++ app/[locale]/accout/signup/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import HeroBlobs from '@/components/HeroBlobs'
 
 const page = () => {
     return (
-        <div className='text-white'>
-            welcome
-        </div>
+        <main className="w-full overflow-x-hidden min-h-screen relative">
+            <Navbar />
+            <HeroBlobs />
+            <section>
+
+            </section>
+            <Footer />
+        </main>
     )
 }
 
