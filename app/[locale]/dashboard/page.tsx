700fdd601da91184df0f92566f3e8db2408ab10b
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2026-07-23 15:37:20 +0330

    HeroBlobs

---
+++ app/[locale]/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroBlobs from '@/components/HeroBlobs'
+const Dashboard = () => {
+    return (
+        <main className='w-full relative h-screen overflow-hidden'>
+            <section className='w-full h-full'>
+                <HeroBlobs />
+            </section>
+        </main>
+    )
+}
+
+export default Dashboard
